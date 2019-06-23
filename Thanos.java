79859568e2decc5d7b5c4118c6b481a265322ac4
Carole Chevalier <ateliercakocako@gmail.com>
2019-06-23 12:46:51 +0200

    nouvelle version

--- Thanos.java
+++ Thanos.java
@@ -5,36 +5,30 @@ class Thanos{
    
     public static void main(String[] args) { 
 
-        ArrayList<Hero> heroes = new ArrayList<Hero>();
+        ArrayList<Hero> heroes = new ArrayList<Hero>(); 
 
-        Hero thor = new Hero("Thor", 1500);
-        heroes.add(thor);
-        Hero blackWidow = new Hero("Black Widow", 34);
-        heroes.add(blackWidow);
-        Hero captainAmerica = new Hero("Captain America", 100);
-        heroes.add(captainAmerica);
-        Hero vision = new Hero("Vision", 3);
-        heroes.add(vision);
-        Hero ironMan = new Hero("Iron Man", 48);
-        heroes.add(ironMan);
-        Hero scarletWich = new Hero("Scarlet Wich", 29);
-        heroes.add(scarletWich);
-        Hero spiderMan = new Hero("Spider-Man", 18);
-        heroes.add(spiderMan);
-        Hero hulk = new Hero("Hulk", 49);
-        heroes.add(hulk);
-        Hero doctorStrange = new Hero("Doctor Strange", 42);
-        heroes.add(doctorStrange);
+        heroes.add(new Hero("Thor", 1501));
+        heroes.add(new Hero("Black Widow", 34));
+        heroes.add(new Hero("Captain America", 100));
+        heroes.add(new Hero("Vision", 3));
+        heroes.add(new Hero("Iron Man", 48));
+        heroes.add(new Hero("Scarlet Wich", 29));
+        heroes.add(new Hero("Spider-Man", 18));
+        heroes.add(new Hero("Hulk", 49));
+        heroes.add(new Hero("Doctor Strange", 42));
 
-        thor.setAge(1501);                  //changement age Thor
+       // Hero thor = new Hero("Thor", 1500);
+        //heroes.add(thor);
+       
+        //Hero.setAge("Thor",(1501));          //changement age Thor"
 
-        Collections.shuffle(heroes);        //melange aleatoire de la liste
+        Collections.shuffle(heroes);            //melange aleatoire de la liste
         
-        heroes.subList(5, heroes.size()).clear();
+        heroes.subList(0, heroes.size()/2).clear();         //ne prend que la moitié du tableau
 
         for(int i = 0; i< heroes.size(); i++){
-            System.out.println(heroes.get(i).getName());       //affiche les heros encore en vie
+            System.out.println(heroes.get(i).getName());           //affiche les heros encore en vie
          }
 
-        }  
-        }
+    }  
+}
